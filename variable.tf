57332883662562e7a34ce441ce22af2096df3076
mohanapriyas1808 <143476042+mohanapriyas1808@users.noreply.github.com>
2024-07-23 00:36:46 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,33 @@
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  default     = 3
+}
+
+variable "security_group_id" {
+  description = "ID of the security group for EC2 instances"
+  
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet for EC2 instances"
+  
+}
+
+variable "key" {
+  description = "Name of the SSH key pair for EC2 instances"
+  
+}
+
+variable "name" {
+  description = "Name prefix for EC2 instances"
+  
+}
+
+variable "default_ec2_tags" {
+  type        = map(string)
+  description = "(optional) default tags for EC2 instances"
+  default = {
+    managed_by   = "terraform"
+    Environment  = "Dev"
+  }
+}
